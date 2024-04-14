f677fdfea1717602e5444f0014e1c38f460bd9db
inaaaa0309 <inaaaa0309@gmail.com>
2024-04-14 17:39:22 +0900

    Initial Commit

---
+++ CPU & Memory MenuBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CPU & Memory MenuBar
+//
+//  Created by 稲谷究 on 2024/04/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
